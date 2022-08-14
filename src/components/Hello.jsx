4ea73d1673cb2bd2d133b457b41e9197e0bb1460
carlosmsx <carlosmsx@gmail.com>
2022-08-14 02:32:46 -0300

    agregado boton Click me y su funcionalidad para setear msj

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -6,6 +6,7 @@ const Hello = (props) => {
     return (
         <div>
             <h1>Hello {props.nombre}{msj}!</h1>
+            <button onClick={()=>setMsj("(from changed State)")}>Click me</button>
         </div>
     );
 };
